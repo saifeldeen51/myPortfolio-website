db6cb24d30f33cc79f554d3e270d923d39290fc4
unknown <saifeldeen51@gmail.com>
2024-05-11 22:26:07 +0300

    Updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/*.css",
+
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
