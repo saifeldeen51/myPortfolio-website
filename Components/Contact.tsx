2f06960a08882d8a8114d8956188ea4bd1de467a
unknown <saifeldeen51@gmail.com>
2024-05-11 20:16:25 +0300

    Updated Contact error row with num

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -17,7 +17,7 @@ const Contact = () => {
           <input type="text" id="name" name="name" placeholder="Your Name" className="border border-[#55e6a5] rounded-md px-4 py-2" />
           <input type="email" id="email" name="email" placeholder="Your Email" className="border border-[#55e6a5] rounded-md px-4 py-2" />
           <input type="text" id="subject" name="subject" placeholder="Subject" className="border border-[#55e6a5] rounded-md px-4 py-2" />
-          <textarea id="message" name="message" placeholder="Your Message" rows="4" className="border border-[#55e6a5] rounded-md px-4 py-2"></textarea>
+          <textarea id="message" name="message" placeholder="Your Message" rows={4} className="border border-[#55e6a5] rounded-md px-4 py-2"></textarea>
           <button type="submit" className="bg-[#55e6a5] text-black px-[2rem] py-[1rem] duration-200 text-[18px] font-bold rounded-md hover:bg-yellow-400 transition-all items-center">Send Message</button>
         </form>
 
